625794ea0b6429ffdbdb899090e403ff352d2885
Alexandru Chimac <36740194+aL31cS@users.noreply.github.com>
2020-09-14 11:54:22 +0300

    Update and rename Heart-vendor.mk to heart-vendor.mk

---
+++ heart-vendor.mk
@@ -0,0 +1,17 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/nokia/heart/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/nokia/heart/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/nokia/heart/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/nokia/heart/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/nokia/heart/vendor/bin,system/vendor/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/nokia/heart/vendor/etc,system/vendor/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/nokia/heart/vendor/firmware,system/vendor/firmware)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/nokia/heart/vendor/lib,system/vendor/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/nokia/heart/vendor/thh,system/vendor/thh)
